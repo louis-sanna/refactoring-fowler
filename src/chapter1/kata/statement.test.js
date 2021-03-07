1d3e79e9b90297e4b8233897331f9eec885d447f
Louis Sanna <louis.sanna@360learning.com>
2021-03-07 12:26:24 +0100

    Write first test on real use-case

--- src/chapter1/kata/statement.test.js
+++ src/chapter1/kata/statement.test.js
@@ -10,4 +10,13 @@ describe('statement', () => {
 
         expect(typeof statement(invoice, plays)).toBe('string');
     });
+    test('should return a string', () => {
+        const invoice = {
+            'customer': 'BigCo',
+            'performances': []
+        };
+        const plays = {};
+
+        expect(statement(invoice, plays)).toBe('Statement for BigCo\nAmount owed is $0.00\nYou earned 0 credits\n');
+    });
 });
